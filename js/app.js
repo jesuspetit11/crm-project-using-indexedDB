1e52f65fb981d4216051dafa0f737d72f458ec0f
Jesus Petit <96022350+jesuspetit11@users.noreply.github.com>
2023-11-13 08:08:47 -0400

    added feature to show the current customers

--- js/app.js
+++ js/app.js
@@ -4,6 +4,10 @@
 
     document.addEventListener("DOMContentLoaded", ()=>{
         crearDB(); //Apenas se carguen todos los elementos del DOM se crea la BD
+
+        if(window.indexedDB.open("crm", 1.0)){
+            obtenerClientes();
+        }
     }); 
     function crearDB() {
 
@@ -34,6 +38,47 @@
             objectStore.createIndex("id", "id", {unique: true});
 
             console.log("Se crearon las filas");
+        }
+    }
+
+    function obtenerClientes() {
+        const abrirConexion = window.indexedDB.open("crm", 1.0);
+        abrirConexion.onerror = function() {
+            console.log("Hubo une error");
+        }
+        abrirConexion.onsuccess = function() {
+            DB = abrirConexion.result;
+
+            const objectStore = DB.transaction("crm").objectStore("crm");
+
+            objectStore.openCursor().onsuccess = function(e){ //Para mostrar los registos de la BD
+                const cursor = e.target.result;
+                if(cursor){
+                    const { nombre, empresa, email, telefono, id } = cursor.value;
+                    const listadoClientes = document.querySelector("#listado-clientes");
+                    listadoClientes.innerHTML += ` 
+                    <tr>
+                    <td class="px-6 py-4 whitespace-no-wrap border-b border-gray-200">
+                        <p class="text-sm leading-5 font-medium text-gray-700 text-lg  font-bold"> ${nombre} </p>
+                        <p class="text-sm leading-10 text-gray-700"> ${email} </p>
+                    </td>
+                    <td class="px-6 py-4 whitespace-no-wrap border-b border-gray-200 ">
+                        <p class="text-gray-700">${telefono}</p>
+                    </td>
+                    <td class="px-6 py-4 whitespace-no-wrap border-b border-gray-200  leading-5 text-gray-700">    
+                        <p class="text-gray-600">${empresa}</p>
+                    </td>
+                    <td class="px-6 py-4 whitespace-no-wrap border-b border-gray-200 text-sm leading-5">
+                        <a href="editar-cliente.html?id=${id}" class="text-teal-600 hover:text-teal-900 mr-5">Editar</a>
+                        <a href="#" data-cliente="${id}" class="text-red-600 hover:text-red-900">Eliminar</a>
+                    </td>
+                </tr>
+                `;
+                    cursor.continue;
+                }else{
+                    console.log("No hay...")
+                }
+            }
         }
     }
 })();
