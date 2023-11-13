9596637f693cb0366c7ce9b3bbfa5beb90fca6b2
Jesus Petit <96022350+jesuspetit11@users.noreply.github.com>
2023-11-13 14:13:00 -0400

    project finished

--- js/app.js
+++ js/app.js
@@ -8,7 +8,33 @@
         if(window.indexedDB.open("crm", 1.0)){
             obtenerClientes();
         }
+        
+        listadoClientes.addEventListener("click", eliminarRegistro);
     }); 
+
+    function eliminarRegistro(e) {
+        if(e.target.classList.contains("eliminar")){
+            const idEliminar = Number(e.target.dataset.cliente);
+
+            const confirmar = confirm("Deseas eliminar?")
+
+            if(confirmar){
+                const transaction = DB.transaction(["crm"],"readwrite");
+                const objectStore = transaction.objectStore("crm");
+                objectStore.delete(idEliminar);
+
+                transaction.oncomplete = function() {
+                    console.log("Eliminado")
+                    e.targe.parentElement.parentElement.remove();
+                }
+                transaction.onerror = function() {
+                    console.log("Hubo un error")
+                }
+            }
+        }
+    }
+
+
     function crearDB() {
 
         //Creamos la BD
@@ -70,7 +96,7 @@
                     </td>
                     <td class="px-6 py-4 whitespace-no-wrap border-b border-gray-200 text-sm leading-5">
                         <a href="editar-cliente.html?id=${id}" class="text-teal-600 hover:text-teal-900 mr-5">Editar</a>
-                        <a href="#" data-cliente="${id}" class="text-red-600 hover:text-red-900">Eliminar</a>
+                        <a href="#" data-cliente="${id}" class="text-red-600 hover:text-red-900 eliminar">Eliminar</a>
                     </td>
                 </tr>
                 `;
